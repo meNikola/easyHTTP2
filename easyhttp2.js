4be438b9d6aa9573ed48b70430413b607bfd59bb
Nikola Kovacevic <kovacvb@hotmail.com>
2018-12-23 14:51:26 +0100

    Create get, post, put, delete method

---
+++ easyhttp2.js
@@ -0,0 +1,60 @@
+class EasyHTTP {
+  // Make GET request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make POST request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make PUT request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  // Make DELETE request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        },
+      })
+        .then(res => res.json())
+        .then(() => resolve('Deleted resource..'))
+        .catch(err => reject(err));
+    });
+  }
+
+
+}
